aa2290bd8fefdf9451b22d065f5cabe4042984c8
Ubuntu <atec@atec.lwsthf5fvliulidrpjgh0c2rwh.bx.internal.cloudapp.net>
2025-04-04 11:37:12 +0000

    just use hashmap

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ mod macedon;
 mod rome;
 mod persia;
 
-use std::io;
+use std::collections::HashMap;
 use std::fs::File;
+use std::io;
 use std::io::{BufRead,BufReader};
-use indexmap::IndexMap;
 use crate::name::Name;
 use crate::name::Name::Genesis;
 use crate::name::Name::Exodus;
@@ -89,18 +89,29 @@ fn print_type_of<T>(_: &T) {
         println!("{}", std::any::type_name::<T>());
 }
 
-fn extract_verse(word: &mut IndexMap<Name, Vec<Vec<String>>>, s: &mut String,
-    b: &mut Vec<u8>, book: Name, chapter: &mut usize, verse: &mut usize, text: &mut String) {
+fn extract_verse(word: &mut HashMap<&Name, Vec<Vec<String>>>, s: &mut String,
+    b: &mut Vec<u8>, book: &Name, chapter: &mut usize, verse: &mut usize, text: &mut String) {
 
+    // TODO(atec): hack at the end
     if s.len() == 0 {
         return
     }
+
     match &s[0..1].parse::<usize>() {
         Ok(n) => {
 
-            word[book][*chapter-1].push(text.clone());
-
             *text = s.clone();
+
+            if let Some(chapter_and_verse) = word.get_mut(book) {
+
+                println!("pushing new text");
+                chapter_and_verse[*chapter-1].push(text.clone());
+            }
+
+            println!("word: {:?}", word);
+
+            let mut tmp = String::new();
+            io::stdin().read_line(&mut tmp).ok().expect("failed to read line");
 
             *verse = *n;
 
@@ -119,7 +130,13 @@ fn extract_verse(word: &mut IndexMap<Name, Vec<Vec<String>>>, s: &mut String,
         },
         Err(e) => {
 
-            println!("failed conversion {}", e);
+            println!("failed conversion extracting verse: {}", e);
+            println!("appending text (probably a colon inside the verse)");
+
+            println!("word: {:?}", word);
+
+            let mut tmp = String::new();
+            io::stdin().read_line(&mut tmp).ok().expect("failed to read line");
 
             *s = String::from_utf8_lossy(&b).to_string();
             *text = text.to_owned() + s;
@@ -139,8 +156,78 @@ fn main() {
 
     let mut r = BufReader::new(File::open("./pg10.txt").expect("can't open file"));
 
-    let book = Genesis;
+    let books = [
+        &Genesis,
+        &Exodus,
+        &Leviticus,
+        &Numbers,
+        &Deuteronomy,
+        &Joshua,
+        &Judges,
+        &Ruth,
+        &SamuelI,
+        &SamuelII,
+        &KingsI,
+        &KingsII,
+        &ChroniclesI,
+        &ChroniclesII,
+        &Ezra,
+        &Nehemiah,
+        &Esther,
+        &Job,
+        &Psalms,
+        &Proverbs,
+        &Ecclesiastes,
+        &SongOfSolomon,
+        &Isaiah,
+        &Jeremiah,
+        &Ezekiel,
+        &Daniel,
+        &Hosea,
+        &Joel,
+        &Amos,
+        &Obadiah,
+        &Jonah,
+        &Micah,
+        &Nahum,
+        &Habakkuk,
+        &Zephaniah,
+        &Haggai,
+        &Zechariah,
+        &Malachi,
+
+        &Matthew,
+        &Mark,
+        &Luke,
+        &John,
+        &Acts,
+        &Romans,
+        &CorinthiansI,
+        &CorinthiansII,
+        &Galatians,
+        &Ephesians,
+        &Philippians,
+        &Colossians,
+        &ThessaloniansI,
+        &ThessaloniansII,
+        &TimothyI,
+        &TimothyII,
+        &Titus,
+        &Philemon,
+        &Hebrews,
+        &James,
+        &PeterI,
+        &PeterII,
+        &JohnI,
+        &JohnII,
+        &JohnIII,
+        &Jude,
+        &Revelation,
+    ];
+
     let mut i: usize = 0;
+    let mut book = books[i];
+
     let mut chapter: usize = 0;
     let mut verse: usize = 0;
     let mut last: usize = 0;
@@ -149,73 +236,73 @@ fn main() {
     let mut started = false;
     let mut text = String::new();
     
-    let mut word = IndexMap::<Name, Vec<Vec<String>>>::from([
-        (Genesis, Vec::new()),
-        (Exodus, Vec::new()),
-        (Leviticus, Vec::new()),
-        (Numbers, Vec::new()),
-        (Deuteronomy, Vec::new()),
-        (Joshua, Vec::new()),
-        (Judges, Vec::new()),
-        (Ruth, Vec::new()),
-        (SamuelI, Vec::new()),
-        (SamuelII, Vec::new()),
-        (KingsI, Vec::new()),
-        (KingsII, Vec::new()),
-        (ChroniclesI, Vec::new()),
-        (ChroniclesII, Vec::new()),
-        (Ezra, Vec::new()),
-        (Nehemiah, Vec::new()),
-        (Esther, Vec::new()),
-        (Job, Vec::new()),
-        (Psalms, Vec::new()),
-        (Proverbs, Vec::new()),
-        (Ecclesiastes, Vec::new()),
-        (SongOfSolomon, Vec::new()),
-        (Isaiah, Vec::new()),
-        (Jeremiah, Vec::new()),
-        (Ezekiel, Vec::new()),
-        (Daniel, Vec::new()),
-        (Hosea, Vec::new()),
-        (Joel, Vec::new()),
-        (Amos, Vec::new()),
-        (Obadiah, Vec::new()),
-        (Jonah, Vec::new()),
-        (Micah, Vec::new()),
-        (Nahum, Vec::new()),
-        (Habakkuk, Vec::new()),
-        (Zephaniah, Vec::new()),
-        (Haggai, Vec::new()),
-        (Zechariah, Vec::new()),
-        (Malachi, Vec::new()),
+    let mut word = HashMap::<&Name, Vec<Vec<String>>>::from([
+        (books[0], Vec::new()),
+        (books[1], Vec::new()),
+        (books[2], Vec::new()),
+        (books[3], Vec::new()),
+        (books[4], Vec::new()),
+        (books[5], Vec::new()),
+        (books[6], Vec::new()),
+        (books[7], Vec::new()),
+        (books[8], Vec::new()),
+        (books[9], Vec::new()),
+        (books[10], Vec::new()),
+        (books[11], Vec::new()),
+        (books[12], Vec::new()),
+        (books[13], Vec::new()),
+        (books[14], Vec::new()),
+        (books[15], Vec::new()),
+        (books[16], Vec::new()),
+        (books[17], Vec::new()),
+        (books[18], Vec::new()),
+        (books[19], Vec::new()),
+        (books[20], Vec::new()),
+        (books[21], Vec::new()),
+        (books[22], Vec::new()),
+        (books[23], Vec::new()),
+        (books[24], Vec::new()),
+        (books[25], Vec::new()),
+        (books[26], Vec::new()),
+        (books[27], Vec::new()),
+        (books[28], Vec::new()),
+        (books[29], Vec::new()),
+        (books[30], Vec::new()),
+        (books[31], Vec::new()),
+        (books[32], Vec::new()),
+        (books[33], Vec::new()),
+        (books[34], Vec::new()),
+        (books[35], Vec::new()),
+        (books[36], Vec::new()),
+        (books[37], Vec::new()),
 
-        (Matthew, Vec::new()),
-        (Mark, Vec::new()),
-        (Luke, Vec::new()),
-        (John, Vec::new()),
-        (Acts, Vec::new()),
-        (Romans, Vec::new()),
-        (CorinthiansI, Vec::new()),
-        (CorinthiansII, Vec::new()),
-        (Galatians, Vec::new()),
-        (Ephesians, Vec::new()),
-        (Philippians, Vec::new()),
-        (Colossians, Vec::new()),
-        (ThessaloniansI, Vec::new()),
-        (ThessaloniansII, Vec::new()),
-        (TimothyI, Vec::new()),
-        (TimothyII, Vec::new()),
-        (Titus, Vec::new()),
-        (Philemon, Vec::new()),
-        (Hebrews, Vec::new()),
-        (James, Vec::new()),
-        (PeterI, Vec::new()),
-        (PeterII, Vec::new()),
-        (JohnI, Vec::new()),
-        (JohnII, Vec::new()),
-        (JohnIII, Vec::new()),
-        (Jude, Vec::new()),
-        (Revelation, Vec::new()),
+        (books[38], Vec::new()),
+        (books[39], Vec::new()),
+        (books[40], Vec::new()),
+        (books[41], Vec::new()),
+        (books[42], Vec::new()),
+        (books[43], Vec::new()),
+        (books[44], Vec::new()),
+        (books[45], Vec::new()),
+        (books[46], Vec::new()),
+        (books[47], Vec::new()),
+        (books[48], Vec::new()),
+        (books[49], Vec::new()),
+        (books[50], Vec::new()),
+        (books[51], Vec::new()),
+        (books[52], Vec::new()),
+        (books[53], Vec::new()),
+        (books[54], Vec::new()),
+        (books[55], Vec::new()),
+        (books[56], Vec::new()),
+        (books[57], Vec::new()),
+        (books[58], Vec::new()),
+        (books[59], Vec::new()),
+        (books[60], Vec::new()),
+        (books[61], Vec::new()),
+        (books[62], Vec::new()),
+        (books[63], Vec::new()),
+        (books[64], Vec::new()),
     ]);
 
     while r.read_until(b':', &mut b).is_ok() {
@@ -226,32 +313,64 @@ fn main() {
             extract_verse(&mut word, &mut s, &mut b, book, &mut chapter, &mut verse, &mut text);
         }
 
+        // TODO(atec): hack at the end
         if s.len() == 0 {
             println!("{:?}", word);
             return
         }
+
         if s.is_char_boundary(s.len()-1) && s.is_char_boundary(s.len()-2) {
 
             // TODO(atec): some recursive bullshit
             match &s[s.len()-2..s.len()-1].parse::<usize>() {
-                Ok(n) => {
+                Ok(n1) => {
 
                     last = chapter;
 
-                    chapter = *n;
-                    word[book].push(Vec::new());
-
                     match &s[s.len()-3..s.len()-1].parse::<usize>() {
-                        Ok(n) => {
-
-                            chapter = *n;
+                        Ok(n2) => {
 
                             match &s[s.len()-4..s.len()-1].parse::<usize>() {
-                                Ok(n) => chapter = *n,
-                                Err(_) => _ = 0,
+                                Ok(n3) => {
+
+                                    chapter = *n3;
+
+                                    if let Some(chapter_and_verse) = word.get_mut(book) {
+
+                                        if chapter != last {
+                                            println!("pushing new chapter and verse");
+                                            chapter_and_verse.push(Vec::new());
+                                        }
+                                    }
+                                },
+                                Err(e) => {
+                                    println!("failed conversion parsing three digit chapter: {}", e);
+                                    println!("falling back to two digits");
+                                    chapter = *n2;
+
+                                    if let Some(chapter_and_verse) = word.get_mut(book) {
+
+                                        if chapter != last {
+                                            println!("pushing new chapter and verse");
+                                            chapter_and_verse.push(Vec::new());
+                                        }
+                                    }
+                                },
                             }
                         },
-                        Err(_) => _ = 0,
+                        Err(e) => {
+                            println!("failed conversion parsing two digit chapter: {}", e);
+                            println!("falling back to one digit");
+                            chapter = *n1;
+
+                            if let Some(chapter_and_verse) = word.get_mut(book) {
+
+                                if chapter != last {
+                                    println!("pushing new chapter and verse");
+                                    chapter_and_verse.push(Vec::new());
+                                }
+                            }
+                        },
                     }
 
                     // TODO(atec): hack to avoid index error on s[0..1]
@@ -266,16 +385,7 @@ fn main() {
 
         if last > chapter {
             i += 1;
-            let (tmp, _) = word.get_index(i).unwrap();
-            book = *tmp;
-            println!("{:?}", word[Genesis]);
-            println!("{:?}", word[Exodus]);
-//            let mut tmp = String::new();
-//            io::stdin().read_line(&mut tmp).ok().expect("failed to read line");
+            book = books[i];
         }
-
-        println!("chapter: {}", chapter);
-        println!("verse: {}", verse);
-        println!("text: {}", text);
     }
 }
