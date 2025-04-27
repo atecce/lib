79b31f249f1f7f05c046d16428c5e53151790b7b
Ubuntu <atec@atec.lwsthf5fvliulidrpjgh0c2rwh.bx.internal.cloudapp.net>
2025-04-27 19:16:10 +0000

    rm superfluous methods

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,6 @@ use crate::greece::macedon::ALEXANDER;
 use crate::rome::CICERO;
 use crate::persia::CYRUS;
 
-fn print_type_of<T>(_: &T) {
-        println!("{}", std::any::type_name::<T>());
-}
-
-fn push_chapter(word: &mut HashMap<&Name, Vec<Vec<String>>>,
-    book: &Name, chapter: &mut usize, last: &mut usize) {
-
-    if let Some(chapter_and_verse) = word.get_mut(book) {
-        if chapter != last {
-            chapter_and_verse.push(Vec::new());
-        }
-    }
-}
-
 fn extract_chapter(s: &mut String) -> Result<usize, ParseIntError> {
     if s.is_char_boundary(s.len()-1) && s.is_char_boundary(s.len()-2) {
         // TODO(atec): some recursive bullshit
@@ -236,7 +222,11 @@ fn read_bible() -> std::io::Result<()> {
                     book = BOOKS[i];
                 }
 
-                push_chapter(&mut word, book, &mut chapter, &mut last);
+                if let Some(chapter_and_verse) = word.get_mut(book) {
+                    if chapter != last {
+                        chapter_and_verse.push(Vec::new());
+                    }
+                }
 
                 // TODO(atec): hack to avoid index error on s[0..1]
                 if !started {
