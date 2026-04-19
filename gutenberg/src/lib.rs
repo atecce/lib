b4d3a47ee0a807eea175ff0d3c9e8ee990508bd1
Alessandro Tecce <root@atec.pub>
2026-04-19 10:09:24 -0500

    mv scrape to gutenberg

--- gutenberg/src/lib.rs
+++ gutenberg/src/lib.rs
@@ -6,8 +6,19 @@ use name::Name::Jude;
 use name::Name::Obadiah;
 use name::Name::Philemon;
 use name::Name::Revelation;
+
 use std::io::{BufRead, BufReader};
 use std::num::ParseIntError;
+
+use futures::{StreamExt, stream};
+
+use reqwest::Client;
+
+use scraper::ElementRef;
+use scraper::Html;
+use scraper::Selector;
+
+const DOMAIN: &str = "https://gutenberg.org";
 
 pub struct Reader<R> {
     r: BufReader<R>,
@@ -206,4 +217,93 @@ impl<R> Reader<R> {
 
         return (verse, s.replace("\r\n", " "));
     }
+}
+
+#[tokio::test]
+async fn scrape() {
+    let client = Client::new();
+
+    let bodies = stream::iter(('a'..='z').map(|c| format!("{}/browse/authors/{}", DOMAIN, c)))
+        .map(|url| {
+            let client = &client;
+            async move {
+                let res = client.get(url).send().await?;
+                res.bytes().await
+            }
+        })
+        .buffer_unordered(10);
+
+    bodies
+        .for_each(|b| async {
+            match b {
+                Ok(b) => {
+                    for element in
+                        Html::parse_document(&String::from_utf8(b.to_vec()).expect("invalid utf8"))
+                            .select(&Selector::parse("h2").unwrap())
+                    {
+                        println!("{:#?}", element.inner_html());
+                        if let Some(next_next_sibling) = element
+                            .next_sibling()
+                            .expect("failed to get next sibling")
+                            .next_sibling()
+                        {
+                            if let Some(next_next_element_ref) = ElementRef::wrap(next_next_sibling)
+                            {
+                                println!(
+                                    "next next sibling: {:?}",
+                                    next_next_element_ref.inner_html()
+                                );
+
+                                let mut urls = Vec::new();
+
+                                for li in
+                                    next_next_element_ref.select(&Selector::parse("li").unwrap())
+                                {
+                                    println!("\t{}", li.inner_html());
+                                    for a in li.select(&Selector::parse("a").unwrap()) {
+                                        println!("\t\t{:?}", a);
+                                        if let Some(href) = a.value().attr("href") {
+                                            println!("\t\t{}", href);
+                                            println!("\t\t{}{}", DOMAIN, href);
+
+                                            let url = format!("{}{}.txt.utf-8", DOMAIN, href);
+                                            println!("\t\t{}", url);
+                                            urls.push(url);
+                                        } else {
+                                            println!("\t\tfailed to find href");
+                                        }
+                                    }
+                                }
+
+                                let bodies = stream::iter(urls)
+                                    .map(|url| {
+                                        let client = &client;
+                                        async move {
+                                            let res = client.get(url).send().await?;
+                                            res.bytes().await
+                                        }
+                                    })
+                                    .buffer_unordered(10);
+
+                                bodies
+                                    .for_each(|b| async {
+                                        match b {
+                                            Ok(b) => println!("{:?}", b),
+                                            Err(e) => eprintln!("e: {}", e),
+                                        }
+                                    })
+                                    .await;
+                            } else {
+                                println!("failed to wrap node with element");
+                                println!("{:#?}", next_next_sibling.value());
+                            }
+                        } else {
+                            println!("failed to find next next sibling");
+                        }
+                    }
+                }
+                Err(e) => eprintln!("e: {}", e),
+            }
+        })
+        .await;
 }
--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,6 @@ use gliner::model::{GLiNER, input::text::TextInput, params::Parameters};
 use gliner::util::result::Result;
 use orp::params::RuntimeParameters;
 
-use futures::{StreamExt, stream};
-
-use reqwest::Client;
-
-use scraper::ElementRef;
-use scraper::Html;
-use scraper::Selector;
-
 fn print_optimates() {
     println!("{:?}", JESUS);
     println!("{:?}", APOLLO);
@@ -55,97 +47,6 @@ fn print_optimates() {
 
 fn main() {
     print_optimates();
-}
-
-const DOMAIN: &str = "https://gutenberg.org";
-
-#[tokio::test]
-async fn scrape() {
-    let client = Client::new();
-
-    let bodies = stream::iter(('a'..='z').map(|c| format!("{}/browse/authors/{}", DOMAIN, c)))
-        .map(|url| {
-            let client = &client;
-            async move {
-                let res = client.get(url).send().await?;
-                res.bytes().await
-            }
-        })
-        .buffer_unordered(10);
-
-    bodies
-        .for_each(|b| async {
-            match b {
-                Ok(b) => {
-                    for element in
-                        Html::parse_document(&String::from_utf8(b.to_vec()).expect("invalid utf8"))
-                            .select(&Selector::parse("h2").unwrap())
-                    {
-                        println!("{:#?}", element.inner_html());
-                        if let Some(next_next_sibling) = element
-                            .next_sibling()
-                            .expect("failed to get next sibling")
-                            .next_sibling()
-                        {
-                            if let Some(next_next_element_ref) = ElementRef::wrap(next_next_sibling)
-                            {
-                                println!(
-                                    "next next sibling: {:?}",
-                                    next_next_element_ref.inner_html()
-                                );
-
-                                let mut urls = Vec::new();
-
-                                for li in
-                                    next_next_element_ref.select(&Selector::parse("li").unwrap())
-                                {
-                                    println!("\t{}", li.inner_html());
-                                    for a in li.select(&Selector::parse("a").unwrap()) {
-                                        println!("\t\t{:?}", a);
-                                        if let Some(href) = a.value().attr("href") {
-                                            println!("\t\t{}", href);
-                                            println!("\t\t{}{}", DOMAIN, href);
-
-                                            let url = format!("{}{}.txt.utf-8", DOMAIN, href);
-                                            println!("\t\t{}", url);
-                                            urls.push(url);
-                                        } else {
-                                            println!("\t\tfailed to find href");
-                                        }
-                                    }
-                                }
-
-                                let bodies = stream::iter(urls)
-                                    .map(|url| {
-                                        let client = &client;
-                                        async move {
-                                            let res = client.get(url).send().await?;
-                                            res.bytes().await
-                                        }
-                                    })
-                                    .buffer_unordered(10);
-
-                                bodies
-                                    .for_each(|b| async {
-                                        match b {
-                                            Ok(b) => println!("{:?}", b),
-                                            Err(e) => eprintln!("e: {}", e),
-                                        }
-                                    })
-                                    .await;
-                            } else {
-                                println!("failed to wrap node with element");
-                                println!("{:#?}", next_next_sibling.value());
-                            }
-                        } else {
-                            println!("failed to find next next sibling");
-                        }
-                    }
-                }
-                Err(e) => eprintln!("e: {}", e),
-            }
-        })
-        .await;
 }
 
 #[test]
