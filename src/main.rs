fe75825ce5d6431c401d890ef014ed18a028f0a0
Alessandro Tecce <root@atec.pub>
2026-04-23 21:58:39 -0500

    prune deps in svc

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,9 @@ use crate::persia::CYRUS;
 use crate::rome::CICERO;
 
 use bible::JESUS;
-use book::Book;
 use daemon::genealogy;
 use greece::APOLLO;
 use greece::macedon::ALEXANDER;
-use name::Name;
-use source::Source;
-
-use std::collections::HashMap;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-
-use gliner::model::pipeline::token::TokenMode;
-use gliner::model::{GLiNER, input::text::TextInput, params::Parameters};
-use gliner::util::result::Result;
-use orp::params::RuntimeParameters;
 
 fn print_optimates() {
     println!("{:?}", genealogy(*JESUS));
@@ -48,68 +36,4 @@ fn print_optimates() {
 
 fn main() {
     print_optimates();
-}
-
-#[test]
-fn things_that_can_be_named() -> Result<()> {
-    let word = bible::io::read_all();
-
-    println!("initiating model...");
-    let model = GLiNER::<TokenMode>::new(
-        Parameters::default(),
-        RuntimeParameters::default(),
-        "./tokenizer.json",
-        "./model.onnx",
-    )?;
-
-    let mut index = HashMap::<String, Vec<Source>>::new();
-
-    let running = Arc::new(AtomicBool::new(true));
-    let r = running.clone();
-
-    ctrlc::set_handler(move || {
-        r.store(false, Ordering::SeqCst);
-    })
-    .expect("setting ctrlc handler");
-
-    for (book, chapter_and_verse) in &word {
-        for (i, chapter) in chapter_and_verse.iter().enumerate() {
-            if !running.load(Ordering::SeqCst) {
-                break;
-            }
-
-            println!("initiating input for {} {}...", book, i + 1);
-            let input = TextInput::new(chapter.to_vec(), vec![String::from("person")])?;
-
-            println!("inferring...");
-            let output = model.inference(input)?;
-            for spans in output.spans {
-                for span in spans {
-                    println!(
-                        "{:3} | {:16} | {:10} | {:.1}%",
-                        span.sequence() + 1,
-                        span.text(),
-                        span.class(),
-                        span.probability() * 100.0,
-                    );
-
-                    let src = Source {
-                        book: Book { name: *book },
-                        chapter: (i + 1).try_into().unwrap(),
-                        verses: [(span.sequence() + 1).try_into().unwrap(), (span.sequence() + 1).try_into().unwrap()],
-                    };
-
-                    if let Some(srcs) = index.get_mut(span.text()) {
-                        srcs.push(src);
-                    } else {
-                        index.insert(span.text().to_string(), vec![src]);
-                    }
-                }
-            }
-        }
-    }
-
-    println!("{:#?}", index);
-
-    Ok(())
 }
