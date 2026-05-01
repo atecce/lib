35c20106f461156e92a67e9cca099a845ee67e41
Alessandro Tecce <root@atec.pub>
2026-04-30 23:42:38 -0500

    move codegen to test

--- bible/tests/kjv.rs
+++ bible/tests/kjv.rs
@@ -150,7 +150,33 @@ pub fn read_all() -> HashMap<Name, Vec<Vec<String>>> {
     word
 }
 
+fn generate_word() {
+    let word = read_all();
+    println!("pub const fn word(book: Name) -> &'static [&'static [&'static str]] {{");
+    println!("    match book {{");
+
+    for book in name::BIBLE {
+        let chapters = &word[&book];
+
+        println!("        name::Name::{} => &[", book);
+        for chapter in chapters {
+            println!("            &[");
+
+            for verse in chapter {
+                let escaped_verse = verse.replace('"', "\\\"");
+                println!("                \"{}\",", escaped_verse);
+            }
+            println!("            ],");
+        }
+        println!("        ],");
+    }
+    println!("        _ => panic!(\"unknown book\"),");
+    println!("    }}");
+    println!("}}");
+}
+
 #[test]
 fn word() {
-    assert!(kjv::get_word() == read_all());
+    generate_word();
+    assert!(kjv::get_word() == read_all())
 }
--- src/main.rs
+++ src/main.rs
@@ -102,29 +102,3 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
-fn generate_word() {
-
-    let word = get_word();
-    println!("pub const fn word(book: Name) -> &'static [&'static [&'static str]] {{");
-    println!("    match book {{");
-
-    for book in name::BIBLE {
-        let chapters = &word[&book];
-
-        println!("        name::Name::{} => &[", book);
-        for chapter in chapters {
-            println!("            &[");
-
-            for verse in chapter {
-                let escaped_verse = verse.replace('"', "\\\"");
-                println!("                \"{}\",", escaped_verse);
-            }
-            println!("            ],");
-        }
-        println!("        ],");
-    }
-    println!("        _ => panic!(\"unknown book\"),");
-    println!("    }}");
-    println!("}}");
-}
