144dca1fdd9f46e4917a726a7463172c0c785810
Alessandro Tecce <root@atec.pub>
2026-05-08 09:32:48 -0500

    construct item from label

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ enum Item {
     PropertyAndEquipmentNet,
     OperatingLeaseAssets,
     Goodwill,
-    IntanglibleAssetsNet,
+    IntangibleAssetsNet,
     DeferredIncomeTaxAssets,
     NonMarketableEquitySecurities,
     OtherAssets,
@@ -64,6 +64,66 @@ enum Item {
 
     IncomeTaxExpense,
     NetIncome,
+}
+
+impl Item {
+    // TODO(atec): start tracking share counts and these stats
+    // Commitments and contingencies - see Note 12 | 2026: 0 | 2025: 0
+    // Preferred stock, $0.001 par value; 2 shares authorized; none issued | 2026: 0 | 2025: 0
+    // outstanding as of January 26, 2025 | 2026: 24 | 2025: 24
+    // Additional paid-in capital | 2026: 10118 | 2025: 11237
+    // Accumulated other comprehensive income | 2026: 178 | 2025: 28
+    // Retained earnings | 2026: 146973 | 2025: 68038
+    // Total shareholders' equity | 2026: 157293 | 2025: 79327
+    // Total liabilities and shareholders' equity | 2026: 206803 | 2025: 111601
+
+    // Basic | 2026: 4.93 | 2025: 2.97
+    // Diluted | 2026: 4.9 | 2025: 2.94
+    // Basic | 2026: 24359 | 2025: 24555
+    // Diluted | 2026: 24514 | 2025: 24804
+    fn from_label(s: &str) -> Option<Self> {
+        match s.trim() {
+            "Cash and cash equivalents" => Some(Item::CashAndCashEquivalents),
+            "Marketable securities" => Some(Item::MarketableSecurities),
+            "Accounts receivable, net" => Some(Item::AccountsReceivableNet),
+            "Inventories" => Some(Item::Inventories),
+            "Prepaid expenses and other current assets" => Some(Item::PrepaidExpensesAndOtherCurrentAssets),
+            "Total current assets" => Some(Item::TotalCurrentAssets),
+            "Property and equipment, net" => Some(Item::PropertyAndEquipmentNet),
+            "Operating lease assets" => Some(Item::OperatingLeaseAssets),
+            "Goodwill" => Some(Item::Goodwill),
+            "Intangible assets, net" => Some(Item::IntangibleAssetsNet),
+            "Deferred income tax assets" => Some(Item::DeferredIncomeTaxAssets),
+            "Non-marketable equity securities" => Some(Item::NonMarketableEquitySecurities),
+            "Other assets" => Some(Item::OtherAssets),
+            "Total assets" => Some(Item::TotalAssets),
+
+            "Accounts payable" => Some(Item::AccountsPayable),
+            "Accrued and other current liabilities" => Some(Item::AccruedAndOtherCurrentLiabilities),
+            "Short-term debt" => Some(Item::ShortTermDebt),
+            "Total current liabilities" => Some(Item::TotalCurrentLiabilities),
+            "Long-term debt" => Some(Item::LongTermDebt),
+            "Long-term operating lease liabilities" => Some(Item::LongTermOperatingLeaseLiabilities),
+            "Other long-term liabilities" => Some(Item::OtherLongTermLiabilities),
+            "Total liabilities" => Some(Item::TotalLiabilities),
+
+            "Revenue" => Some(Item::Revenue),
+            "Cost of revenue" => Some(Item::CostOfRevenue),
+            "Gross profit" => Some(Item::GrossProfit),
+            "Research and development" => Some(Item::ResearchAndDevelopment),
+            "Sales, general and administrative" => Some(Item::SalesGeneralAndAdministrative),
+            "Total operating expenses" => Some(Item::TotalOperatingExpenses),
+            "Operating income" => Some(Item::OperatingIncome),
+            "Interest income" => Some(Item::InterestIncome),
+            "Interese expense" => Some(Item::InterestExpense),
+            "Other income, net" => Some(Item::OtherIncomeNet),
+            "Total other income, net" => Some(Item::TotalOtherIncomeNet),
+            "Income before income tax" => Some(Item::IncomeBeforeIncomeTax),
+            "Income tax expense" => Some(Item::IncomeTaxExpense),
+            "Net income" => Some(Item::NetIncome),
+            &_ => todo!(),
+        }
+    }
 }
 
 struct ReportedItem {
