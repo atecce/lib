812ea1af3c5a2062748a2e71794eca9bca605df2
Alessandro Tecce <root@atec.pub>
2026-05-14 16:42:25 -0500

    break out mod for genealogy

--- bible/examples/yeshua.rs
+++ bible/examples/yeshua.rs
@@ -1,12 +1,12 @@
 fn main() {
-    for src in bible::JESUS.words {
+    for src in bible::genealogy::JESUS.words {
         println!("{}", src);
         let text = &bible::kjv::word(src.book)[src.chapter as usize - 1]
             [src.verses[0] as usize - 1..=src.verses[1] as usize - 1];
         println!("{:?}", text);
     }
 
-    for deed in bible::JESUS.deeds {
+    for deed in bible::genealogy::JESUS.deeds {
         println!("desc: {}", deed.desc);
         for src in deed.srcs {
             println!("{}", src);
