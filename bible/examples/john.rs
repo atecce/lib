7e40a81d8531186d96da0234523f5b14ae3d5c34
Alessandro Tecce <root@atec.pub>
2026-05-31 22:31:59 -0500

    john

---
+++ bible/examples/john.rs
@@ -0,0 +1,20 @@
+fn main() {
+    for src in [
+        bible::Source {
+            book: name::Name::Genesis,
+            chapter: 28,
+            start: 10,
+            end: Some(22),
+        }
+    ] {
+        let verses = bible::kjv::word(src.book)[src.chapter as usize - 1];
+        println!("{}", src);
+        let text: &[&str];
+        if let Some(end) = src.end {
+            text = &verses[src.start as usize - 1..=end as usize - 1];
+        } else {
+            text = &verses[src.start as usize - 1..src.start as usize];
+        }
+        println!("{:#?}", text);
+    }
+}
