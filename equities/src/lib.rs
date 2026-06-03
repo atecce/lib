18805407d728ca87e03f333ef419b94429987a72
Alessandro Tecce <root@atec.pub>
2026-06-03 10:35:26 -0500

    extract balance sheet as its own type

--- equities/src/lib.rs
+++ equities/src/lib.rs
@@ -9,6 +9,60 @@ use crate::item::Item;
 
 // use chrono::NaiveDate;
 
+pub struct BalanceSheet {
+
+    cash_and_cash_equivalents: f64,
+    marketable_securities: f64,
+    accounts_receivable_net: f64,
+    inventories: f64,
+    prepaid_expenses_and_other_current_assets: f64,
+
+    property_and_equipment_net: f64,
+    operating_lease_assets: f64,
+    goodwill: f64,
+    intangible_assets_net: f64,
+    deferred_income_tax_assets: f64,
+    other_assets: f64,
+
+    accounts_payable: f64,
+    accrued_and_other_current_liabilities: f64,
+    short_term_debt: f64,
+
+    long_term_debt: f64,
+    long_term_operating_lease_liabilities: f64,
+    other_long_term_liabilities: f64,
+}
+
+impl BalanceSheet {
+    fn total_current_assets(&self) -> f64 {
+        self.cash_and_cash_equivalents
+            + self.marketable_securities
+            + self.accounts_receivable_net
+            + self.inventories
+            + self.prepaid_expenses_and_other_current_assets
+    }
+    fn total_assets(self) -> f64 {
+        self.total_current_assets()
+            + self.property_and_equipment_net
+            + self.operating_lease_assets
+            + self.goodwill
+            + self.intangible_assets_net
+            + self.deferred_income_tax_assets
+            + self.other_assets
+    }
+    fn total_current_liabilities(&self) -> f64 {
+        self.accounts_payable
+            + self.accrued_and_other_current_liabilities
+            + self.short_term_debt
+    }
+    fn total_liabilities(self) -> f64 {
+        self.total_current_liabilities()
+            + self.long_term_debt
+            + self.long_term_operating_lease_liabilities
+            + self.other_long_term_liabilities
+    }
+}
+
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, uniffi::Enum)]
 pub enum Period {
     ThreeMonths,
