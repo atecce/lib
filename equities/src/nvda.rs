b3c3f18d4d49dd3efecf652e54c0d32c4830e805
Alessandro Tecce <root@atec.pub>
2026-06-10 00:56:05 -0500

    git add src/nvda.rs

---
+++ equities/src/nvda.rs
@@ -0,0 +1,65 @@
+use crate::Ticker;
+use crate::BalanceSheet as Type;
+
+use chrono::NaiveDate;
+
+pub struct BalanceSheet {
+    pub date: NaiveDate,
+
+    pub cash_and_cash_equivalents: f64,
+    pub marketable_securities: f64,
+    pub accounts_receivable_net: f64,
+    pub inventories: f64,
+    pub prepaid_expenses_and_other_current_assets: f64,
+
+    pub property_and_equipment_net: f64,
+    pub operating_lease_assets: f64,
+    pub goodwill: f64,
+    pub intangible_assets_net: f64,
+    pub deferred_income_tax_assets: f64,
+    pub other_assets: f64,
+
+    pub accounts_payable: f64,
+    pub accrued_and_other_current_liabilities: f64,
+    pub short_term_debt: f64,
+
+    pub long_term_debt: f64,
+    pub long_term_operating_lease_liabilities: f64,
+    pub other_long_term_liabilities: f64,
+}
+
+impl Type for BalanceSheet {
+    fn ticker(&self) -> Ticker {
+        Ticker::NVDA
+    }
+}
+
+impl BalanceSheet {
+    pub fn total_current_assets(&self) -> f64 {
+        self.cash_and_cash_equivalents
+            + self.marketable_securities
+            + self.accounts_receivable_net
+            + self.inventories
+            + self.prepaid_expenses_and_other_current_assets
+    }
+    pub fn total_assets(&self) -> f64 {
+        self.total_current_assets()
+            + self.property_and_equipment_net
+            + self.operating_lease_assets
+            + self.goodwill
+            + self.intangible_assets_net
+            + self.deferred_income_tax_assets
+            + self.other_assets
+    }
+    pub fn total_current_liabilities(&self) -> f64 {
+        self.accounts_payable
+            + self.accrued_and_other_current_liabilities
+            + self.short_term_debt
+    }
+    pub fn total_liabilities(&self) -> f64 {
+        self.total_current_liabilities()
+            + self.long_term_debt
+            + self.long_term_operating_lease_liabilities
+            + self.other_long_term_liabilities
+    }
+}
